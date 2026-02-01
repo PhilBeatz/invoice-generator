488b9fc83c405c987fe25da2d0437051f5f5ad56
PhilBeatz <phildouthard@gmail.com>
2026-02-01 18:29:29 -0500

    Add Contact route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import InvoiceGenerator from './components/InvoiceGenerator';
+import Contact from './components/Contact';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<InvoiceGenerator />} />
         <Route path="/invoicegenerator" element={<InvoiceGenerator />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
